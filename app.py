bb2b17a0a8148f3f211634ceff2b678d06273012
hrishikesh1320 <154308794+hrishikesh1320@users.noreply.github.com>
2024-05-07 14:08:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="Medical Plant Detection")
+
+st.header("Medical Plant Detection App")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Detect the Plant")
+
+input_prompt="""
+You are an expert in plant biologists where you need to see the plant  from the image
+               and provide the details of the plant with Biological name 
+               is below format
+
+               1. Biological Name - Biological Name /n
+               2. Usage - Biological Name  /n
+               3. Area - where the plant found /n
+               
+        Finally you have to also mention in which disease the plant use.
+
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("Predicted Output..")
+    st.write(response)
